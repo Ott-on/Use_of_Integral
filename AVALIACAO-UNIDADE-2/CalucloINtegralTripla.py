78d95516db818cc303aed88f36ba6dfbcc7bfecc
Otton <134803634+Ott-on@users.noreply.github.com>
2024-06-12 23:15:31 -0300

    Update CalucloINtegralTripla.py

--- AVALIACAO-UNIDADE-2/CalucloINtegralTripla.py
+++ AVALIACAO-UNIDADE-2/CalucloINtegralTripla.py
@@ -2,24 +2,27 @@ import scipy.integrate as integrate
 import numpy as np
 
 # Informar ao usuário sobre a unidade de medida esperada
-print("Por favor, insira o raio e a altura do cilindro em centímetros (cm).")
+print("Por favor, insira o raio e a altura do inseticida em centímetros (cm).")
 
-# Solicitar ao usuário para inserir o raio e a altura do cilindro
-R = float(input("Digite o raio do cilindro: "))  # Raio em cm
-h = float(input("Digite a altura do cilindro: "))  # Altura em cm
+try:
+    h = float(input("Informe a altura do inseticida em centímetros: "))
+    R = float(input("Informe o raio do inseticida em centímetros: "))
+except ValueError:
+    print("Erro: Por favor, insira valores numéricos para a altura e o raio do inseticida.")
+    exit()
 
 # Função a ser integrada
 def integrand(r, theta, z):
     return r
 
 # Executar a integração tripla
-volume, error = integrate.tplquad(integrand,
+volume, error = integrate.tplquad(integrand, 
                                   0, h,        # Limites para z
                                   lambda z: 0, lambda z: 2*np.pi,  # Limites para theta
                                   lambda z, theta: 0, lambda z, theta: R)  # Limites para r
 
-# Formatar o volume para mostrar no máximo dois dígitos após a vírgula
-volume_formatado = f"{volume:.2f}"
+volume_litros = volume / 1000  # Convertendo para litros
+volume_mililitros = volume_litros * 1000  # Convertendo para mililitros
 
 # Imprimir o volume calculado com formatação
-print(f"Volume do cilindro com raio {R} cm e altura {h} cm é: {volume_formatado} cm³")
+print(f'Volume do inseticida: {volume:.2f} cm³\nRaio: {R:.2f} cm\nAltura: {h:.2f} cm\nVolume: {volume_litros:.2f} L\nVolume: {volume_mililitros:.2f} mL')
